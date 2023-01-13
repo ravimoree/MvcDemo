dcc3ae6e9ba5e82bdbe80f4b97268074e61f1b5f
Ravi <moreeravi@gmail.com>
2023-01-13 17:11:49 +0530

    Add project files.

---
+++ MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string UserName, string Password)
+        {
+            if (UserName == "admin" && Password == "admin")
+            {
+                return RedirectToAction("Country");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult State()
+        {
+            return View();
+        }
+        public ActionResult Country()
+        {
+            return View();
+        }
+    }
+}
